e49ca5c22454253c0c981d4426f4141f83f6e9e1
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-05-16 15:16:57 -0700

    update

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,4 +1,10 @@
 <!DOCTYPE HTML>
 <html>
-    <script> src="Hello_World.js":</script>
+    <script> src="Basic_JavaScript_1.js":
+    function My_First_Function() {
+        var String = "Kiss me, I'm Irish!";
+        var result = String.fontcolor("green");
+        document.getElementByID("Irish").innerHTML =
+    }
+    </script>
 </html>
